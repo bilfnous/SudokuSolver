6fa695505165270875c545f2c77f0cea78d737d4
B. Alfanous <b.alfanous@outlook.com>
2020-04-08 21:16:38 -0400

    added print_grid function

--- sudoku_model.py
+++ sudoku_model.py
@@ -7,13 +7,31 @@
 # __file__ sudoku_model.py
 
 
+grid_m = [[0 for i in range(9)] for i in range(9)]
+
+def print_grid(grid_m):
+    print("- - - - - - - - - - - - - - - - -")
+    for i in range(len(grid_m)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - - - - -")
+
+        for j in range(len(grid_m[0])):
+            if j % 3 == 0 and j != 0:
+                print(" |  ", end="")
+
+            if j == 8:
+                print(str(grid_m[i][j]) + " ")
+            else:
+                print(str(grid_m[i][j]) + "  ", end="")
     
+    print("- - - - - - - - - - - - - - - - -")
     
 def main():
-    grid_m = [[0 for i in range(9)] for i in range(9)]
+    print_grid(grid_m)
     print("Fill in the sudoku grid; enter 0 to fill the empty square: ")
     for i in range(9):
         for j in range(9):
             grid_m[i][j] = input(f'[{i + 1}] [{j + 1}] >> ')
+            
 
 if __name__ == '__main__': main()
