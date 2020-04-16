743c26e62b2002b8acee5c128eabe8fcd1d5c857
B. Alfanous <b.alfanous@outlook.com>
2020-04-16 16:52:57 -0400

    Added the print functionality

--- sudoku_view.py
+++ sudoku_view.py
@@ -5,3 +5,21 @@
 # __email__ b.alfanous@outlook.com
 # __date__ 28/Mar/2020
 # __file__ sudoku_view.py
+
+
+def print_grid(grid):
+    print("- - - - - - - - - - - - - - - - -")
+    for i in range(len(grid)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - - - - -")
+
+        for j in range(len(grid[0])):
+            if j % 3 == 0 and j != 0:
+                print(" |  ", end="")
+
+            if j == 8:
+                print(str(grid[i][j]) + " ")
+            else:
+                print(str(grid[i][j]) + "  ", end="")
+    
+    print("- - - - - - - - - - - - - - - - -")
