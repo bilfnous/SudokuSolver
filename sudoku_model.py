68d85d3e65f8c40fef9f0c38fddc326f239f5011
B. Alfanous <b.alfanous@outlook.com>
2020-04-16 16:54:59 -0400

    Added two solve functions

--- sudoku_model.py
+++ sudoku_model.py
@@ -6,32 +6,36 @@
 # __date__ 28/Mar/2020
 # __file__ sudoku_model.py
 
+import sudoku_controller as controller
+import sudoku_view as view
 
 grid_m = [[0 for i in range(9)] for i in range(9)]
+possibilties_m = [[ [] for i in range(9)] for i in range(9)]
 
-def print_grid(grid_m):
-    print("- - - - - - - - - - - - - - - - -")
-    for i in range(len(grid_m)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - - - - -")
+def solve_grid():
+    for row in range(9):
+        for col in range(9):
+            if grid_m[row][col] == 0 :
+                possibilties_m[row][col] = get_possibilties(row, col)
 
-        for j in range(len(grid_m[0])):
-            if j % 3 == 0 and j != 0:
-                print(" |  ", end="")
-
-            if j == 8:
-                print(str(grid_m[i][j]) + " ")
-            else:
-                print(str(grid_m[i][j]) + "  ", end="")
-    
-    print("- - - - - - - - - - - - - - - - -")
-    
-def main():
-    print_grid(grid_m)
-    print("Fill in the sudoku grid; enter 0 to fill the empty square: ")
+def get_possibilties(row, col):
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in range(9):
-        for j in range(9):
-            grid_m[i][j] = input(f'[{i + 1}] [{j + 1}] >> ')
-            
+        if grid_m[row][i] != 0:
+            if grid_m[row][i] in numbers:
+                numbers.remove(grid_m[row][i])
+        
+        if grid_m[i][col] != 0:
+            if grid_m[i][col] in numbers:
+                numbers.remove(grid_m[i][col])
 
+
+    return numbers
+
+
+def main():
+    controller.read_grid(grid_m)
+    solve_grid()
+    view.print_grid(grid_m)
+            
 if __name__ == '__main__': main()
