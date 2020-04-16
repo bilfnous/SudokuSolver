2cab3458823a1dea78694ef0575f42b33ae1fa40
B. Alfanous <b.alfanous@outlook.com>
2020-04-16 16:53:47 -0400

    Added read function

--- sudoku_controller.py
+++ sudoku_controller.py
@@ -6,3 +6,8 @@
 # __date__ 28/Mar/2020
 # __file__ sudoku_controller.py
 
+def read_grid(grid):
+    print("Fill in the sudoku grid; enter 0 to fill the empty square: ")
+    for i in range(9):
+        for j in range(9):
+            grid[i][j] = input(f'[{i + 1}] [{j + 1}] >> ')
